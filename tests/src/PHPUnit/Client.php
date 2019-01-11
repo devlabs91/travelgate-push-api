ff7fc88e83e6f299ff776c84265f18e06721ffc6
Andy A <anders.a9010@gmail.com>
2019-01-11 17:22:39 +0700

    Cleanup in Client

--- tests/src/PHPUnit/Client.php
+++ tests/src/PHPUnit/Client.php
@@ -2,21 +2,13 @@
 
 namespace Devlabs91\TravelgatePushApi\Tests\PHPUnit;
 
-class Client extends \SoapClient
-{
+class Client extends \SoapClient {
     
     public $mockResponse;
     
-    public function __doRequest($request, $location, $action, $version, $one_way = NULL)
-    {
+    public function __doRequest($request, $location, $action, $version, $one_way = NULL) {
         if( $this->mockResponse ) { return $this->mockResponse; }
-/*        if( stristr($request, 'HotelResRetrieve') ) {
-            return file_get_contents(__DIR__ . '/../../resources/HotelResRetrieveRS.xml');
-        }
-        else if( stristr($request, 'HotelRatePlanInventoryRetrieve') ) {
-            return file_get_contents(__DIR__ . '/../../resources/HotelRatePlanInventoryRetrieveRS.xml');
-        }
-*/        return parent::__doRequest($request, $location, $action, $version, $one_way);
+        return parent::__doRequest($request, $location, $action, $version, $one_way);
     }
     
     public function setMockResponse($mockResponse) {
